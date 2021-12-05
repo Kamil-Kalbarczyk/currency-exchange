f554f3ee0d4f92da23c072908370f4a9d2320b31
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2021-12-05 19:46:13 +0100

    render and filter currences add with JS code, CSS styles and html

---
+++ js/code.js
@@ -0,0 +1,46 @@
+let dataBase;
+
+const currencyList = document.querySelector('.currencyList');
+currencyList.innerHTML = "";
+
+try {
+    let response = await fetch('http://api.nbp.pl/api/exchangerates/tables/A/');
+    let data = await response.json();
+    dataBase = data;
+} catch (error) {
+    currencyList.textContent = 'Błąd importu danych'
+
+};
+
+
+
+const currencyRates = dataBase[0].rates;
+const currencyRatesDate = dataBase[0].effectiveDate;
+
+// render currences
+
+const renderCurrences = (currences) => {
+    currences.forEach((currency) => {
+        const li = document.createElement('li');
+        const divCurrencySymbol = document.createElement('div');
+        const divCurrencyRate = document.createElement('div');
+        divCurrencySymbol.textContent = currency.code;
+        divCurrencyRate.textContent = currency.mid;
+        li.append(divCurrencySymbol, divCurrencyRate);
+        currencyList.append(li);
+    })
+};
+
+renderCurrences(currencyRates);
+
+const currencySearchInput = document.querySelector('.currencyListContainer input');
+
+currencySearchInput.addEventListener('keyup', () => {
+
+    currencyList.innerHTML = "";
+    const searchCurrency = currencyRates.filter((currency) => {
+        return currency.code.toLowerCase().includes(currencySearchInput.value.toLowerCase())
+    });
+
+    renderCurrences(searchCurrency);
+})
